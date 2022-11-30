237e27c71dbf89f7b0b8dee3c63499840324890f
Eometheous <jonathan@sthomasphoto.com>
2022-11-29 20:57:29 -0800

    Initial commit

---
+++ src/main/java/main/trees/Node.java
@@ -0,0 +1,48 @@
+package main.trees;
+
+import java.util.Objects;
+
+public class Node {
+    private Node parent;
+    private Node leftChild;
+    private Node rightChild;
+    private String word;
+    private boolean black;
+
+    public Node() {
+        leftChild = null;
+        rightChild = null;
+        word = "";
+        black = true;
+    }
+
+    public boolean equals(Node node) {
+        return Objects.equals(this.getWord(), node.getWord());
+    }
+
+    public Node getParent() {return  parent;}
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+    public Node getLeftChild() {return leftChild;}
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+    public Node getRightChild() {return rightChild;}
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+    public String getWord() {return word;}
+    public void setWord(String word) {
+        this.word = word;
+    }
+    public boolean getColor() {return black;}
+    public void setColor(boolean black) {
+        this.black = black;
+    }
+    public boolean isLeaf() {return leftChild == null && rightChild == null;}
+
+    public int compareTo(Node parent) {
+        return word.compareTo(parent.getWord());
+    }
+}
---
+++ src/main/java/main/trees/RedBlackTree.java
@@ -0,0 +1,126 @@
+package main.trees;
+
+import java.util.Objects;
+
+public class RedBlackTree {
+    private Node root;
+
+    public RedBlackTree() {
+        root = null;
+    }
+
+    public boolean isLeaf(Node n){
+        if (n.equals(root) && n.getLeftChild() == null && n.getRightChild() == null) return true;
+        if (n.equals(root)) return false;
+        return n.getLeftChild() == null && n.getRightChild() == null;
+    }
+
+    public interface Visitor{
+        /**
+         This method is called at each node.
+         @param n the visited node
+         */
+        void visit(Node n);
+    }
+
+    public void visit(Node n){
+        System.out.println(n.getWord());
+    }
+
+    public void printTree(){  //preorder: visit, go left, go right
+        Node currentNode = root;
+        printTree(currentNode);
+    }
+
+    public void printTree(Node node){
+        System.out.print(node.getWord());
+        if (node.isLeaf()){
+            return;
+        }
+        printTree(node.getLeftChild());
+        printTree(node.getRightChild());
+    }
+
+    // place a new node in the RB tree with data the parameter and color it red.
+    public void addNode(String data){  	//this < that  <0.  this > that  >0
+        //	fill
+
+    }
+
+    public void insert(String data){
+        addNode(data);
+    }
+
+    public Node lookup(String k){
+        Node searching = root;
+        while (searching != null) {
+            if (Objects.equals(searching.getWord(), k)) return searching;
+            if (searching.getWord().compareTo(k) < 0)
+                searching = searching.getLeftChild();
+            if (searching.getWord().compareTo(k) > 0) {
+                searching = searching.getRightChild();
+            }
+        }
+        return null;
+    }
+
+
+    public Node getSibling(Node n){
+        if (n.getParent().getRightChild() == n) {
+            return n.getParent().getLeftChild();
+        }
+        return n.getParent().getRightChild();
+    }
+
+
+    public Node getAunt(Node n){
+        if (isLeftChild(getGrandparent(n), n.getParent())) {
+            return getGrandparent(n).getRightChild();
+        }
+        return getGrandparent(n).getLeftChild();
+    }
+
+    public Node getGrandparent(Node n){
+        return n.getParent().getParent();
+    }
+
+    public void rotateLeft(Node n){
+        //
+    }
+
+    public void rotateRight(Node n){
+        //
+    }
+
+    public void fixTree(Node current) {
+        //
+    }
+
+    public boolean isEmpty(Node n){
+        return n.getWord() == null;
+    }
+
+    public boolean isLeftChild(Node parent, Node child)
+    {
+        //child is less than parent
+        return child.compareTo(parent) < 0;
+    }
+
+    public void preOrderVisit(Visitor v) {
+        preOrderVisit(root, v);
+    }
+
+
+    private static void preOrderVisit(Node n, Visitor v) {
+        if (n == null) {
+            return;
+        }
+        v.visit(n);
+        preOrderVisit(n.getLeftChild(), v);
+        preOrderVisit(n.getRightChild(), v);
+    }
+
+    public Node getRoot() { return root;}
+    public void setRoot(Node root) { this.root = root;}
+
+}
