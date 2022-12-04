a28a7bec10333d653aa3367e0a319b423beec533
Eometheous <jonathan.sthomas@icloud.com>
2022-12-04 12:20:54 -0800

    Added some testing

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import main.trees.Node;
+import main.trees.RedBlackTree;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RedBlackTree rbt = new RedBlackTree();
+        rbt.insert("g");
+        rbt.insert("h");
+        rbt.insert("b");
+        rbt.insert("a");
+        rbt.insert("c");
+        rbt.printTree();
+        rbt.rotateRight(rbt.getRoot());
+        System.out.println("");
+        rbt.printTree();
     }
 }
