0881d027392db22bee8afb55060694f11b026916
Eometheous <jonathan.sthomas@icloud.com>
2022-12-04 13:14:45 -0800

    reverted changes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,13 +6,13 @@ import main.trees.RedBlackTree;
 public class Main {
     public static void main(String[] args) {
         RedBlackTree rbt = new RedBlackTree();
+        rbt.insert("g");
         rbt.insert("b");
         rbt.insert("a");
-        rbt.insert("g");
         rbt.insert("c");
         rbt.insert("h");
         rbt.printTree();
-        rbt.rotateLeft(rbt.getRoot());
+        rbt.rotateRight(rbt.getRoot());
         System.out.println("");
         rbt.printTree();
 
