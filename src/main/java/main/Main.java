51d0f1a9dc571cc5b7052ddb0586ccc672a5d93c
Eometheous <jonathan.sthomas@icloud.com>
2022-12-06 15:59:16 -0800

    Removed commented code

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,28 +1,13 @@
 package main;
 
-import main.trees.Node;
 import main.trees.RedBlackTree;
-
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
         RedBlackTree rbt = new RedBlackTree();
         rbt.loadDictionary();
-        //
         rbt.spellCheck();
-
-        //Printing dictionary causes issues
-        //rbt.printTree();
-        rbt.rotateRight(rbt.getRoot());
-        System.out.println("");
-        //rbt.printTree();
-
-        rbt.rotateLeft(rbt.getRoot());
-        System.out.println("");
-        //bt.printTree();
     }
 }
